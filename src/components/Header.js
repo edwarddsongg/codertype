4ee454cfd16f3664b52943b27d4de0f7858882ea
edward-song-life-is-short <edwarddsongg@gmail.com>
2022-02-09 15:06:28 -0500

    hover nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,30 @@ import { Link, useParams } from 'react-router-dom';
 
 import './Header.css';
 
-
-
-const HeaderLink = ({ page }) => {
+const HeaderLink = ({ page, selected }) => {
   const title = page.charAt(0).toUpperCase() + page.slice(1);
+  let className = selected ? 'headerlink-no-link ' : '';
+  className += 'headerlink-title';
 
-  return <Link to={`/${page}`}>{title}</Link>;
+  return (
+    <Link to={`/${page}`} className={className}>
+      {title}
+      <div className={selected ? 'headerlink-dot-active' : 'headerlink-dot'}>
+        •
+      </div>
+    </Link>
+  );
 };
 
 const Header = () => {
+  const page = useParams().page || 'home';
+
   return (
     <div className='header'>
-      <HeaderLink page='home' />
-      <HeaderLink page='about' />
-      <HeaderLink page='contact' />
+      <HeaderLink page='home' selected={page === 'home'} />
+      <HeaderLink page='code' selected={page === 'code'} />
+      <HeaderLink page='standings' selected={page === 'standings'} />
+      <HeaderLink page='contact' selected={page === 'contact'} />
     </div>
   );
 };
