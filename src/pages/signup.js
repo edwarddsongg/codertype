5f749a3a077b6268e2adab8e96bb4f1c674e99c7
edward-song-life-is-short <edwarddsongg@gmail.com>
2022-02-16 10:51:53 -0500

    building login page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,66 +1,44 @@
+import React from 'react'
 
-import React from 'react';
-
-class Example extends React.Component {
-  constructor() {
-    super();
-    this.state = { time: {}, seconds: 5 };
-    this.timer = 0;
-    this.startTimer = this.startTimer.bind(this);
-    this.countDown = this.countDown.bind(this);
-  }
-
-  secondsToTime(secs){
-    let hours = Math.floor(secs / (60 * 60));
-
-    let divisor_for_minutes = secs % (60 * 60);
-    let minutes = Math.floor(divisor_for_minutes / 60);
-
-    let divisor_for_seconds = divisor_for_minutes % 60;
-    let seconds = Math.ceil(divisor_for_seconds);
-
-    let obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
-    };
-    return obj;
-  }
-
-  componentDidMount() {
-    let timeLeftVar = this.secondsToTime(this.state.seconds);
-    this.setState({ time: timeLeftVar });
-  }
-
-  startTimer() {
-    if (this.timer == 0 && this.state.seconds > 0) {
-      this.timer = setInterval(this.countDown, 1000);
-    }
-  }
-
-  countDown() {
-    // Remove one second, set state so a re-render happens.
-    let seconds = this.state.seconds - 1;
-    this.setState({
-      time: this.secondsToTime(seconds),
-      seconds: seconds,
-    });
-    
-    // Check if we're at zero.
-    if (seconds == 0) { 
-      clearInterval(this.timer);
-    }
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
   render() {
-    return(
-      <div>
-        <button onClick={this.startTimer}>Start</button>
-        m: {this.state.time.m} s: {this.state.time.s}
+    <div className="main_contain">
+      <div className="box-contain">
+        <div className="box-control">
+          <div className="control">
+            Login
+          </div>
+            Register
+        </div>
       </div>
-    );
+    </div>
   }
 }
 
+class LoginBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
-export default Example;
+  render() {
+    <div className="inner-box">
+      <div className="input_group">
+        <label htmlFor="usernmae"> Password</label>
+        <input type="text" name="username" className="login_input" placeholder="Username" />
+      </div>
+
+      <div className="input_group">
+        <label htmlFor="password"> Password</label>
+        <input type="password" name="password" className="login_input" placeholder="Password" />
+      </div>
+    </div>
+  }
+}
+
+export default Signup;
